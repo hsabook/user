b627fbf48ed4554f8749f5717bbac8237c183f44
dattx <dattx@mirabo-global.com>
2025-04-04 10:12:52 +0700

    fix: build cicd

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -11,24 +11,10 @@ import {
 } from '@/components/ui/toast';
 
 export function Toaster() {
-  const { toasts } = useToast();
+  const { toast } = useToast();
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
       <ToastViewport />
     </ToastProvider>
   );
